97619afe54af28d41e006a843ffc64ac9f3a7337
Luciano Vernaschi <luciano@vaadin.com>
2022-09-29 09:51:09 +0200

    Configure plugin with custom non-null annotation

--- library/src/main/java/com/example/application/endpoints/helloworld/HelloWorldEndpoint.java
+++ library/src/main/java/com/example/application/endpoints/helloworld/HelloWorldEndpoint.java
@@ -1,15 +1,15 @@
 package com.example.application.endpoints.helloworld;
 
+import com.example.application.annotations.NeverNull;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 import dev.hilla.Endpoint;
-import dev.hilla.Nonnull;
 
 @Endpoint
 @AnonymousAllowed
 public class HelloWorldEndpoint {
 
-    @Nonnull
-    public String sayHello(@Nonnull String name) {
+    @NeverNull
+    public String sayHello(@NeverNull String name) {
         if (name.isEmpty()) {
             return "Hello stranger";
         } else {
